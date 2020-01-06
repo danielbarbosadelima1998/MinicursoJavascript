e297f4e91627a5e54933f009af3c3109f3f94fc0
Daniel Barbosa de Lima <daniel@mandeumzap.com.br>
2020-01-06 17:13:09 -0300

    novas functions

--- src/App.js
+++ src/App.js
@@ -87,6 +87,130 @@ function App() {
         Concat: {A.concat(B).map((item, index) => <li key={index}>[{index}] = {item}</li>)}
       </div>
 
+      <div>
+        <h1>Função copyWithin</h1>
+        <p> copia parte de um array para outra localização dentro deste mesmo array e o retorna</p>
+        ['a','b','c','d','e','f','g','h', 'i', 'j', 'k', 'l', 'm', 'n'].copyWithin(0, 3);
+        Resultado:
+
+        {['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'].copyWithin(0, 3).map((item) => <li>{item}</li>)}
+      </div>
+      <div>
+        <h1>Função every</h1>
+        <p> percorre todos os elementos do array e executa uma arrow function pra cada elemento, se todas as funções retornarem true, a função every tambem retorna true senão retorna false</p>
+
+        [1, 2, 3, 4, 5, 6].every(item => item > 4)
+
+        <br /><br />Retorno da função: {[1, 2, 3, 4, 5, 6].every(item => item > 4) ? <p>true</p> : <p>false</p>}
+      </div>
+
+      <div>
+        <h1>função Fill</h1>
+        <p>preenche todos os valores do array a partir de um inicio até um final com um valor. Ele irá mudar o objeto em si, e retorná-lo</p>
+        [1,2,3,4,5,6,7,8,9,10]<br />
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].fill(0, 1, 3)
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].fill(0, 1, 3).map(item => <li>{item}</li>)}
+      </div>
+
+      <div>
+        <h1>Filter</h1>
+        <p> cria um novo array com todos os elementos que passaram na condição implementado pela arrow function.</p>
+        [0,1,2,3,4,5,6,7,8]
+        <p>item >= 5</p>
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].filter(item => item >= 5).map(item => <li>{item}</li>)}
+      </div>
+      <div>
+        <h1>Find</h1>
+        <p>retorna o primeiro elemento que o retorno da arrow function for true, caso contrario undefined</p>
+        [1,2,3,4,5,6,7]
+        [1,2,3,4,5,6,7].find(item => item > 3)
+        <li>{[1, 2, 3, 4, 5, 6, 7].find(item => item > 3)}</li>
+      </div>
+      <div>
+        <h1>findIndex</h1>
+        <p> retorna o índice no array do primeiro elemento que satisfizer a arrow function função. Caso contrário, retorna -1.</p>
+        [1,2,3,4,5,6,7,8]
+        <br />
+        [1, 2, 3, 4, 5, 6, 7, 8].findIndex(item => item === 5)
+        <li>{[1, 2, 3, 4, 5, 6, 7, 8].findIndex(item => item === 5)}</li>
+      </div>
+
+      <div>
+        <h1>Flat</h1>
+        <p>retorna a concatenação de arrays dentro de arrays de forma recursiva até onde for delimitado </p>
+        [1,2,3.4,5,6,7,8] <br />
+        [1, 2, 3, 4, 5, 6, 7, 8, [12,13,14,15,[66,77,88,99,[99,88,665,34234]]]].flat(3)
+        {[1, 2, 3, 4, 5, 6, 7, 8, [12, 13, 14, 15, [66, 77, 88, 99, [99, 88, 665, 34234]]]].flat(3).map(item => <li>{item}</li>)}
+        <br />
+
+        [1, 2, 3, 4, 5, 6, 7, 8, [1, 2, 3, 4, 5]].flat() //1
+        {[1, 2, 3, 4, 5, 6, 7, 8, [1, 2, 3, 4, 5]].flat(1).map(item => <li> {item}</li>)}
+      </div>
+
+      <div>
+        <h1>forEach</h1>
+        <p>executa uma função pra cada elemento</p>
+
+        [10,20,30,40,50,60]
+        <br />
+        Resultado no console
+      {[10, 20, 30, 40, 50, 60].forEach((item, index) => console.log(`${index} ola mundo ${item}`))}
+      </div>
+
+      <div>
+        <h1>Includes</h1>
+        <p>retorna true ou false se o elemento existir no array</p>
+        [1,3,4,57,7,89,9] <br /> <br />
+        [1,3,4,57,7,89,9].includes(58) <br />
+        Resultado:
+        {[1, 3, 4, 57, 7, 89, 9].includes(58) ? 'true' : 'false'} <br />
+
+        <br />
+        [1,3,4,57,7,89,9].includes(57) <br />
+        Resultado:
+        {[1, 3, 4, 57, 7, 89, 9].includes(57) ? 'true' : 'false'}
+      </div>
+      <div>
+        <h1>indexOf</h1>
+        <p>retorna o indice do primeiro elemento encontrado no array caso não seja encontrado retorna -1</p>
+
+        [5,7,9,5,3,6,9] <br />
+        [5,7,9,5,3,6,9].indexOf(9) <br />
+        Resultado: {[5, 7, 9, 5, 3, 6, 9].indexOf(9)} <br />
+
+        [5,7,9,5,3,6,9].indexOf(8) <br />
+        Resultado: {[5, 7, 9, 5, 3, 6, 9].indexOf(8)}
+      </div>
+      <div>
+        <h1>join</h1>
+        <p>transforma todos os elementos de um array em uma string, os elementos podem ser separados por algum caractere</p>
+        [32,32.1321,5,8,89,7,8,5,4,5,52]
+        {[32, 32, 1321, 5, 8, 89, 7, 8, 5, 4, 5, 5].map(item => <li>{item}</li>)}
+        [32,32,1321,5,8,89,7,8,5,4,5,5].join() <br />
+        Resultado:
+        {[32, 32, 1321, 5, 8, 89, 7, 8, 5, 4, 5, 5].join()}<br /><br />
+        Resultado:
+        [32, 32, 1321, 5, 8, 89, 7, 8, 5, 4, 5, 5].join(-) <br />
+        {[32, 32, 1321, 5, 8, 89, 7, 8, 5, 4, 5, 5].join('-')}
+        <br />
+        <br />
+
+        *N possibilidades de separação
+      </div>
+
+      <div>
+        <h1> keys</h1>
+        <p>retorna um array com os indices do array</p>
+        [9,8,7,6,5,4,3,35,6,6,4,45,56,77,7,6,54] <br />
+        Resultado:
+        {Object.keys([9, 8, 7, 6, 5, 4, 3, 35, 6, 6, 4, 45, 56, 77, 7, 6, 54]).map(item => <li>{item}</li>)}
+      </div>
+      <div>
+        <h1> reverse</h1>
+        <p> reverte a posição de todos os elementos do array (123... => 321...) e mantem os indices</p>
+        [1,2,3,4,5,6]
+      {[1, 2, 3, 4, 5, 6].reverse().map((item,index) => <li> [{index}] = {item}</li>)}
+      </div>
     </div>
   );
 }
